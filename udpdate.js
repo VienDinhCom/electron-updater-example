19205999de9f638c873ac9cd9c33680dd971619d
Maxvien <maxvien@dinh.local>
2018-09-09 15:59:29 +0700

    U

--- udpdate.js
+++ udpdate.js
@@ -4,37 +4,42 @@ const { exec } = require('child_process');
 const { dialog } = require('electron');
 
 module.exports = function(app) {
+  if (os.platform() !== 'darwin') return;
 
-  if (os.platform() === 'darwin') {
+  const appBasePath = app.getPath('exe').split('.app')[0];
+  const appFullPath = appBasePath + '.app';
+  const appName = appBasePath.split('/')[appBasePath.split('/').length - 1];
+  const appParentsPath = appFullPath.replace(`${appName}.app`, '')
 
-    const appBasePath = app.getPath('exe').split('.app')[0];
-    const appFullPath = appBasePath + '.app';
-    const appName = appBasePath.split('/')[appBasePath.split('/').length - 1];
-    const appParentsPath = appFullPath.replace(`${appName}.app`, '')
-  
-    const appBakName = appName + '.bak';
-    const appBakFullPath = appFullPath.replace(`${appName}.app`, `${appBakName}.app`);
-  
-    const updateFolder = app.getPath('userData') + '/__update__';
-    const updateInfo = JSON.parse(fs.readFileSync(`${updateFolder}/update-info.json`, 'utf8')).fileName;
-    const updateFile = updateFolder + '/' + updateInfo;
+  const appBakName = appName + ' Old Version';
+  const appBakFullPath = appFullPath.replace(`${appName}.app`, `${appBakName}.app`);
 
-    dialog.showMessageBox({message: appFullPath});
-  
-    exec(`mv "${appFullPath}" "${appBakFullPath}"`, (error, stdout, stderr) => {
+  const updateFolder = app.getPath('userData') + '/__update__';
+  const updateInfo = JSON.parse(fs.readFileSync(`${updateFolder}/update-info.json`, 'utf8')).fileName;
+  const updateFile = updateFolder + '/' + updateInfo;
+
+  dialog.showMessageBox({message: app.getVersion()})
+
+  exec(`rm -rf "${updateFolder}/app"`, (error, stdout, stderr) => {
+    if (error || stderr.trim()) return dialog.showMessageBox({message: stderr});
+
+    exec(`unzip -o "${updateFile}" -d "${updateFolder}/app"`, (error, stdout, stderr) => {
       if (error || stderr.trim()) return dialog.showMessageBox({message: stderr});
-  
-      exec(`unzip -o "${updateFile}" -d "${appParentsPath}"`, (error, stdout, stderr) => {
+
+      exec(`mv "${appFullPath}" "${appBakFullPath}"`, (error, stdout, stderr) => {
         if (error || stderr.trim()) return dialog.showMessageBox({message: stderr});
-  
-        exec(`rm -rf "${appBakFullPath}"`, (error, stdout, stderr) => {
+
+        exec(`cd "${updateFolder}/app" && mv * "${appParentsPath}"`, (error, stdout, stderr) => {
           if (error || stderr.trim()) return dialog.showMessageBox({message: stderr});
-          exec(process.execPath);
-          app.quit();
+
+          exec(`rm -rf "${appBakFullPath}"`, (error, stdout, stderr) => {
+            if (error || stderr.trim()) return dialog.showMessageBox({message: stderr});
+  
+            exec(`"${app.getPath('exe')}"`);
+            app.quit();
+          });
         });
       });
     });
-    
-  }
-
+  });
 }
